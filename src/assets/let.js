dcfb186b4b4634059a3023479f21e9ea4e431a5f
Idan <IdanArgaman@gmail.com>
2021-04-09 16:47:04 +0300

    Delete unrelated example

--- src/assets/let.js
+++ src/assets/let.js
@@ -8,37 +8,5 @@ export default [{
     "categoryId": CodeTypesEnum.LET,
     "title": "Understanding ES6 let",
     "description": "",
-    "code": () => {
-        (() => {
-            'use strict'
-            {
-                let _value = 'secret'
-                function nested() {
-                    return _value;
-                }
-            }
-
-            // ---------- Works in ES5 but it’s broken in ES6
-
-            try {
-                console.log(nested()) // nested is not defined
-            } catch (e) {
-                console.log(e)
-            }
-
-        })();
-
-        (() => {
-            {
-                let _value = 'secret'
-                function nested() {
-                    return _value;
-                }
-            }
-
-            // ---------- Works in ES5 but it’s broken in ES6
-
-            console.log(nested()) // nested is not defined
-        })();
-    }
+    "code": ""
 }];
